ff9d5306d2ef090107c3271db48ab0a0fb2737be
ignacio salazar <ignaciosalazar986@gmail.com>
2026-05-15 22:21:20 -0300

    feat: update theme color in index.html and manifest.json; add service worker for caching assets

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,15 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
     <App />
-  
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
